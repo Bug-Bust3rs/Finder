f25a1767d60f9b9829cdfcdfff0bee8fc5173a2a
Puskar-Roy <puskarroy600@gmail.com>
2024-07-21 01:53:07 +0530

    feat : schadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Button } from "@/components/ui/button"
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Button>CLick Me</Button>
     </>
   )
 }
