93bbe65716d306a2a06dbba35ce81b3d2871210a
Puskar-Roy <puskarroy600@gmail.com>
2024-07-22 00:46:16 +0530

    feat : server 2

---
+++ packages/node_server1/src/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cookie = require('cookie-parser')
+const compression = require('compression')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const CheckError = require('./utlis/checkError')
+const limiter = require('./utlis/rateLimitter')
+const addLogger = require('./utlis/appLogger')
+// const { testRoute } = require('./controllers/authController')
+const { config } = require('./configs/config')
+// const authRouter = require('./routes/authRoutes')
+const app = express()
+// require('./database/connectDB')
+
+app.use(express.json())
+app.use(compression())
+app.use(mongoSanitize())
+app.use(helmet())
+app.use(limiter)
+app.use(cookie())
+
+addLogger(app)
+
+app.use((req, res, next) => {
+    res.setHeader('X-XSS-Protection', '1; mode=block')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({ status: true })
+})
+// app.use('/api/auth', authRouter)
+app.all('*', (req, res) => {
+    const error = new CheckError(
+        `Can't find ${req.originalUrl} on this server!`,
+        404
+    )
+    res.status(error.statusCode).json({
+        success: false,
+        error: error.message,
+    })
+})
+
+app.listen(config.PORT, () => {
+    console.log(`[⚡] Server Is Running on http://localhost:${config.PORT}`)
+})
+
+module.exports = { app }
